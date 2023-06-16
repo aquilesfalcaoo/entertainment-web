70d0abc1465d84fa7c44a690e3c780e83a1be1ec
Aquiles <aquilesfalcaoo@gmail.com>
2023-06-15 21:42:38 -0300

    feat: inicializando um novo projeto angular 16

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,ts}"];
+export const theme = {
+  colors: {
+    red: "#FC4747",
+    "dark-blue": "#10141E",
+    "greyish-blue": "#5A698F",
+    "semi-dark-blue": "#161D2F",
+  },
+  extend: {},
+};
+export const plugins = [];
