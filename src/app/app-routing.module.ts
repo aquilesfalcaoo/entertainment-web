bdaafc97403ed185b7e11fc2ee1ec30305d05e36
Aquiles <aquilesfalcaoo@gmail.com>
2023-09-01 08:14:43 -0300

    hotfix: fix em app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
